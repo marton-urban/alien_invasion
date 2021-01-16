e351e0336fae8ed3008884c125e003232ba9c687
Márton Urbán <urban.marton@gmail.com>
2021-01-16 22:34:21 +0100

    Moved all doc strings one up.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,25 +1,21 @@
 import sys
-
 import pygame
-
 from settings import Settings
 
-
+"""Overall class to manage game assets and behavior."""
 class AlienInvasion:
-    """Overall class to manage game assets and behavior."""
-
+    """Initialize the game, and create game resources."""
     def __init__(self):
-        """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+    """Start the main loop for the game."""
     def run_game(self):
-        """Start the main loop for the game."""
+        # Watch for keyboard and mouse event.
         while True:
-            # Watch for keyboard and mouse event.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
@@ -30,7 +26,7 @@ class AlienInvasion:
             pygame.display.flip()
 
 
+# Make a game instance, and run the game.
 if __name__ == '__main__':
-    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
+"""A class to store all settings for Alien Invasion."""
 class Settings:
-    """A class to store all settings for Alien Invasion."""
-
+    """Initialize the game's settings."""
     def __init__(self):
-        """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 700
