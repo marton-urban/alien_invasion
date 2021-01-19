907f3194d9051842705632bbbb67af5fb3848af9
Márton Urbán <urban.marton@gmail.com>
2021-01-19 14:01:18 +0100

    Game Over!

--- game_stats.py
+++ game_stats.py
@@ -5,7 +5,10 @@ class GameStats:
     def __init__(self, ai_game):
         self.settings = ai_game.settings
         self.reset_stats()
+        # Start Alien Invasion in an active state.
+        self.game_active = True
 
     """Initialize statistics that can change during the game."""
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
+
